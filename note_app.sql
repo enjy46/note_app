ed1918b5eb0b41f14d94fb028fb5435ef0083f53
enjy46 <enjyy.mowafy@gmail.com>
2025-05-02 04:11:46 +0300

    adjust sql file

--- note_app.sql
+++ note_app.sql
@@ -11,7 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -34,15 +33,6 @@ CREATE TABLE `notes` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `notes`
---
-
-INSERT INTO `notes` (`id`, `user_id`, `content`, `created_at`) VALUES
-(1, 1, 'test note', '2025-05-02 00:45:38'),
-(2, 1, 'hii', '2025-05-02 00:45:46'),
-(3, 1, 'byeee', '2025-05-02 00:45:48');
-
 -- --------------------------------------------------------
 
 --
@@ -55,56 +45,36 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
+-- --------------------------------------------------------
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'jee', '$2y$10$j.ZPkL9cMj/Wjn2aCMrALO4QmibubkfumHfYsPmVHyWzPaQUN60HK');
-
---
 -- Indexes for dumped tables
---
 
---
 -- Indexes for table `notes`
---
 ALTER TABLE `notes`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
---
 -- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
---
 -- AUTO_INCREMENT for dumped tables
---
 
---
 -- AUTO_INCREMENT for table `notes`
---
 ALTER TABLE `notes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
---
 -- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
---
 -- Constraints for dumped tables
---
 
---
 -- Constraints for table `notes`
---
 ALTER TABLE `notes`
   ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
